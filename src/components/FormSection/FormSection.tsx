a9dada0981b74982b0703804f6198899c663e7a2
IlyaDulyuk <dulyuk20051712@gmail.com>
2024-12-23 09:47:52 +0300

    add padding

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -1,7 +1,7 @@
 import { Button, Input, Section, Textarea} from '@telegram-apps/telegram-ui';
 
 export const FormSection = () => (
-  <Section header="Заполните форму ниже" footer={<Button size="l" mode='filled' stretched>Добавить</Button>}>
+  <Section header="Заполните форму ниже" footer={<div style={{padding: '0px 20px', marginTop: '20px'}}><Button size="l" mode='filled' stretched>Добавить</Button></div>}>
     <Input header="Груз" placeholder="Тип груза" /> 
     <Input header="Вес" placeholder="Общий вес посылки в кг" /> 
     <Input header="Цена за кг" placeholder="Стоимость доставки за килограмм" /> 
