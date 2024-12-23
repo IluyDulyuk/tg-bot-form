afec211f7cd00ad78afa0d766a9dd09e9f188cf2
IlyaDulyuk <dulyuk20051712@gmail.com>
2024-12-23 09:39:00 +0300

    add  footer

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -1,13 +1,12 @@
 import { Button, Input, Section, Textarea} from '@telegram-apps/telegram-ui';
 
 export const FormSection = () => (
-  <Section header="Заполните форму ниже">
+  <Section header="Заполните форму ниже" footer={<Button size="l" mode='filled' stretched>Добавить</Button>}>
     <Input header="Груз" placeholder="Тип груза" /> 
     <Input header="Вес" placeholder="Общий вес посылки в кг" /> 
     <Input header="Цена за кг" placeholder="Стоимость доставки за килограмм" /> 
     <Input header="Откуда" placeholder="Город отправления" /> 
     <Input header="Куда" placeholder="Город назначения" />
     <Textarea header="Комментарий" placeholder="Дополнительные детали" />
-    <Button size="l" stretched>Добавить</Button>
   </Section>
 );
