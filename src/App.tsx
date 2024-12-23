b4fc21d0f3ec36350428e4468f54dbb247716b7d
IlyaDulyuk <dulyuk20051712@gmail.com>
2024-12-23 12:34:04 +0300

    add select

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import { AppRoot, List } from '@telegram-apps/telegram-ui';
 import { FormSection } from './components/FormSection/FormSection';
-// import { ModalSection } from './components/ModalSection/ModalSection';
 
 export const App = () => (
   <AppRoot>
     <List>
       <FormSection />
-      {/* <ModalSection /> */}
     </List>
   </AppRoot>
 );
--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -1,12 +1,35 @@
-import { Button, Input, Section, Textarea} from '@telegram-apps/telegram-ui';
+import { Button, Input, Section, Select, Textarea } from '@telegram-apps/telegram-ui';
+import { useState } from 'react';
 
-export const FormSection = () => (
-  <Section header="Заполните форму ниже" footer={<div style={{padding: '0px 20px', marginTop: '20px'}}><Button size="l" mode='filled' stretched>Добавить</Button></div>}>
-    <Input header="Груз" placeholder="Тип груза" /> 
-    <Input header="Вес" placeholder="Общий вес посылки в кг" /> 
-    <Input header="Цена за кг" placeholder="Стоимость доставки за килограмм" /> 
-    <Input header="Откуда" placeholder="Город отправления" /> 
-    <Input header="Куда" placeholder="Город назначения" />
-    <Textarea header="Комментарий" placeholder="Дополнительные детали" />
-  </Section>
-);
+export const FormSection = () => {
+
+  const [type, setType] = useState('');
+  const [weight, setWeight] = useState('');
+
+  return (
+    <Section
+      header="Заполните форму ниже"
+      footer={
+        <div style={{ padding: '0px 20px', margin: '20px 0px' }
+        }>
+          <Button
+            disabled
+            size="l"
+            mode='filled'
+            stretched
+          >Добавить</Button>
+        </div>
+      }>
+      <Input header="Груз" placeholder="Тип груза" value={type} onChange={(e) => setType(e.target.value)} />
+      <Input header="Вес" placeholder="Общий вес посылки в кг" value={weight} onChange={(e) => setWeight(e.target.value)} />
+      <Input header="Цена за кг" placeholder="Стоимость доставки за килограмм" />
+      {/* <Input header="Откуда" placeholder="Город отправления" />
+      <Input header="Куда" placeholder="Город назначения" /> */}
+      <Select header="Откуда">
+        <option>Москва</option>
+        <option>Санкт-Петербург</option>
+      </Select>
+      <Textarea header="Комментарий" placeholder="Дополнительные детали" />
+    </Section>
+  )
+};
