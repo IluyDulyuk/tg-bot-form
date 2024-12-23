83170c2da96ceed0e803706ff4eaf20d408d24c4
IlyaDulyuk <dulyuk20051712@gmail.com>
2024-12-23 12:57:45 +0300

    add layout

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -1,4 +1,4 @@
-import { Input, Select, Textarea } from '@telegram-apps/telegram-ui';
+import { Button, Input, Section, Select, Textarea } from '@telegram-apps/telegram-ui';
 import { useState } from 'react';
 
 export const FormSection = () => {
@@ -8,16 +8,32 @@ export const FormSection = () => {
 
   return (
     <>
-      <Input header="Груз" placeholder="Тип груза" value={type} onChange={(e) => setType(e.target.value)} />
-      <Input header="Вес" placeholder="Общий вес посылки в кг" value={weight} onChange={(e) => setWeight(e.target.value)} />
-      <Input header="Цена за кг" placeholder="Стоимость доставки за килограмм" />
-      {/* <Input header="Откуда" placeholder="Город отправления" />
-      <Input header="Куда" placeholder="Город назначения" /> */}
-      <Select header="Откуда">
-        <option>Москва</option>
-        <option>Санкт-Петербург</option>
-      </Select>
-      <Textarea header="Комментарий" placeholder="Дополнительные детали" />
+      <Section header="Общая информация">
+        <Input header="Груз" placeholder="Тип груза" value={type} onChange={(e) => setType(e.target.value)} />
+        <Input header="Вес" placeholder="Общий вес посылки в кг" value={weight} onChange={(e) => setWeight(e.target.value)} />
+        <Input header="Цена за кг" placeholder="Стоимость доставки за килограмм" />
+      </Section>
+      <Section header="Откуда">
+        <Select>
+          <option>Москва</option>
+          <option>Красноярск</option>
+        </Select>
+      </Section>
+      <Section header="Куда">
+        <Select>
+          <option>Москва</option>
+          <option>Красноярск</option>
+        </Select>
+      </Section>
+      <Section header="Дополнительно">
+        <Textarea header="Комментарий" placeholder="Дополнительные детали" />
+      </Section>
+      <div style={{padding: '20px'}}>
+      <Button
+          size='l'
+          stretched
+        >Добавить</Button>
+      </div>
     </>
   )
 };
