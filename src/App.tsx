56b8a8bf56d8a35bd882b2b85162a93125b682e3
IlyaDulyuk <dulyuk20051712@gmail.com>
2024-12-24 16:43:47 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { FormSection } from './components/FormSection/FormSection';
 
 export const App = () => (
   <AppRoot>
-    <div style={{maxHeight: '100dvh', overflowY: 'auto'}}>
+    <div style={{maxHeight: '100dvh', overflowY: 'auto', height: '100dvh'}}>
       <FormSection/>
     </div>
   </AppRoot>
--- src/components/FormSection/FormSection.jsx
+++ src/components/FormSection/FormSection.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, Section, Select, Textarea } from '@telegram-apps/telegram-ui';
+import { Button, Input, List, Section, Select, Textarea } from '@telegram-apps/telegram-ui';
 import {  useEffect, useState } from 'react';
 import WebApp from '@twa-dev/sdk';
 
@@ -83,26 +83,28 @@ export const FormSection = () => {
         userData ?
         (
           <>
-            <Section header="Общая информация">
-              <Input header="Груз" placeholder="Тип груза" value={type} onChange={(e) => setType(e.target.value)} />
-              <Input header="Вес" placeholder="Общий вес посылки в кг" value={weight} onChange={(e) => setWeight(e.target.value.replace(/[^\d.]/g, '').replace(/(\..*?)\./g, '$1'))} onFocus={onWeightFocus} onBlur={onWeightBlur} />
-              <Input header="Цена за кг" placeholder="Стоимость доставки за кг" value={price} onChange={(e) => setPrice(e.target.value.replace(/[^\d.]/g, '').replace(/(\..*?)\./g, '$1'))} onFocus={onPriceFocus} onBlur={onPriceBlur} />
-            </Section>
-            <Section header="Откуда">
-              <Select value={from} onChange={(e) => setFrom(e.target.value)}>
-                <option>Москва</option>
-                <option>Санкт-Петербург</option>
-              </Select>
-            </Section>
-            <Section header="Куда">
-              <Select value={to} onChange={(e) => setTo(e.target.value)} >
-                <option>Москва</option>
-                <option>Санкт-Петербург</option>
-              </Select>
-            </Section>
-            <Section header="Дополнительно">
-              <Textarea value={comment} onChange={(e) => setComment(e.target.value)} header="Комментарий" placeholder="Дополнительные детали" />
-            </Section>
+            <List>
+              <Section header="Общая информация">
+                <Input header="Груз" placeholder="Тип груза" value={type} onChange={(e) => setType(e.target.value)} />
+                <Input header="Вес" placeholder="Общий вес посылки в кг" value={weight} onChange={(e) => setWeight(e.target.value.replace(/[^\d.]/g, '').replace(/(\..*?)\./g, '$1'))} onFocus={onWeightFocus} onBlur={onWeightBlur} />
+                <Input header="Цена за кг" placeholder="Стоимость доставки за кг" value={price} onChange={(e) => setPrice(e.target.value.replace(/[^\d.]/g, '').replace(/(\..*?)\./g, '$1'))} onFocus={onPriceFocus} onBlur={onPriceBlur} />
+              </Section>
+              <Section header="Откуда">
+                <Select value={from} onChange={(e) => setFrom(e.target.value)}>
+                  <option>Москва</option>
+                  <option>Санкт-Петербург</option>
+                </Select>
+              </Section>
+              <Section header="Куда">
+                <Select value={to} onChange={(e) => setTo(e.target.value)} >
+                  <option>Москва</option>
+                  <option>Санкт-Петербург</option>
+                </Select>
+              </Section>
+              <Section header="Дополнительно">
+                <Textarea value={comment} onChange={(e) => setComment(e.target.value)} header="Комментарий" placeholder="Дополнительные детали" />
+              </Section>
+            </List>
             <div style={{padding: '20px'}}>
             <Button
                 size='l'
