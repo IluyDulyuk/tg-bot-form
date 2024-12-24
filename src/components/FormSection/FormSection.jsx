6c493c6ed39c818d0e0af6da285960867edbaef0
IlyaDulyuk <dulyuk20051712@gmail.com>
2024-12-24 19:12:42 +0300

    commit

--- src/components/FormSection/FormSection.jsx
+++ src/components/FormSection/FormSection.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, List, Section, Textarea } from '@telegram-apps/telegram-ui';
+import { Button, Cell, Input, List, Section, Select, Switch, Textarea } from '@telegram-apps/telegram-ui';
 import {  useEffect, useState } from 'react';
 import WebApp from '@twa-dev/sdk';
 
@@ -17,9 +17,13 @@ export const FormSection = () => {
   const [weight, setWeight] = useState('');
   const [price, setPrice] = useState('');
   const [from, setFrom] = useState('');
-  const [comment, setComment] = useState('');
   const [to, setTo] = useState('');
+  const [fromSelect, setFromSelect] = useState('Москва');
+  const [toSelect, setToSelect] = useState('Казань');
+  const [comment, setComment] = useState('');
   const [loading, setLoading] = useState(false);
+  const [noFrom, setNoFrom] = useState(false);
+  const [noTo, setNoTo] = useState(false);
 
   const onWeightFocus = () => {
     const value = weight.split(' ')[0];
@@ -51,7 +55,8 @@ export const FormSection = () => {
 
     const data = {
       user: {
-        chatId: userData.id
+        chatId: userData.id,
+        username: userData.username
       },
       data: {
         type,
@@ -80,7 +85,7 @@ export const FormSection = () => {
   return (
     <>
       {
-        userData ?
+        1 ?
         (
           <>
             <List>
@@ -90,10 +95,66 @@ export const FormSection = () => {
                 <Input header="Цена за кг" placeholder="Стоимость доставки за кг" value={price} onChange={(e) => setPrice(e.target.value.replace(/[^\d.]/g, '').replace(/(\..*?)\./g, '$1'))} onFocus={onPriceFocus} onBlur={onPriceBlur} />
               </Section>
               <Section header="Откуда">
-                <Input placeholder="Город отправления" value={from} onChange={(e) => setFrom(e.target.value)} />
+
+                {
+                  noFrom ? 
+                  (
+                    <Input placeholder="Город отправления" value={from} onChange={(e) => setFrom(e.target.value)} />
+                  ) :
+                  (
+                    <Select value={fromSelect} onChange={(e) => setFromSelect(e.target.value)}>
+                      <option>Москва</option>
+                      <option>Медина</option>
+                      <option>Мекка</option>
+                      <option>Эр-Рияд</option>
+                      <option>Джидда</option>
+                      <option>Каир</option>
+                      <option>Стамбул</option>
+                      <option>Анкара</option>
+                      <option>Махачкала</option>
+                      <option>Грозный</option>
+                      <option>Казань</option>
+                    </Select>
+                  )
+                }
+
+                <Cell
+                  Component="label"
+                  after={<Switch value={noFrom} onChange={() => setNoFrom(!noFrom)} />}
+                >
+                  Города нет в списке?
+                </Cell>
               </Section>
               <Section header="Куда">
-                <Input placeholder="Город доставки" value={to} onChange={(e) => setTo(e.target.value)} />
+
+                {
+                  noTo ? 
+                  (
+                    <Input placeholder="Город доставки" value={to} onChange={(e) => setTo(e.target.value)} />
+                  ) :
+                  (
+                    <Select value={toSelect} onChange={(e) => setToSelect(e.target.value)}>
+                      <option>Москва</option>
+                      <option>Медина</option>
+                      <option>Мекка</option>
+                      <option>Эр-Рияд</option>
+                      <option>Джидда</option>
+                      <option>Каир</option>
+                      <option>Стамбул</option>
+                      <option>Анкара</option>
+                      <option>Махачкала</option>
+                      <option>Грозный</option>
+                      <option>Казань</option>
+                    </Select>
+                  )
+                }
+
+                <Cell
+                  Component="label"
+                  after={<Switch value={noTo} onChange={() => setNoTo(!noTo)} />}
+                >
+                  Города нет в списке?
+                </Cell>
               </Section>
               <Section header="Дополнительно">
                 <Textarea value={comment} onChange={(e) => setComment(e.target.value)} header="Комментарий" placeholder="Дополнительные детали" />
@@ -103,7 +164,7 @@ export const FormSection = () => {
             <Button
                 size='l'
                 stretched
-                disabled={type === '' || weight === '' || price === '' || from === '' || to === ''}
+                disabled={type === '' || weight === '' || price === '' || (noFrom && from === '' || !noFrom && fromSelect === '') || (noTo && to === '' || !noTo && toSelect === '')}
                 onClick={onButton}
                 loading={loading}
               >Создать объявление</Button>
